6ed94e07f1870cebce5c1a0dcdf507a018449a98
jtrebolledom <jtrebolledom@gmail.com>
2020-09-15 23:22:49 -0300

    First deploy without functions

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";//import vars of hook
+import "./App.css";//import css
+
+
+function App() {//function Principal
+  
+  return ( /*return this value*/
+    <div className="calculator-app"> {/* div principal*/}
+      
+    </div>
+  );
+}
+export default App;
