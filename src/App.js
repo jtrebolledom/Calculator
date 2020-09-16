195211bd6e22c16c18a6e06b2d1734723cd53722
jtrebolledom <jtrebolledom@gmail.com>
2020-09-16 00:03:59 -0300

    include the function components, and modify the app.js file

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from "react";//import vars of hook
 import "./App.css";//import css
-
+import Calculator from "./Components/Calculator";
 
 function App() {//function Principal
   
   return ( /*return this value*/
     <div className="calculator-app"> {/* div principal*/}
-      
+      <Calculator/>
     </div>
   );
 }
