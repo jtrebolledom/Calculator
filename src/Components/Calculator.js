534bc13935e7518e9c246f87e7fe158392b8423d
jtrebolledom <jtrebolledom@gmail.com>
2020-09-16 11:48:49 -0300

    // functions handleClick and buttons calculator

--- src/Components/Calculator.js
+++ src/Components/Calculator.js
@@ -1,12 +1,15 @@
 import React, {useState, useRef} from "react";//import vars of hook
 
-
 function Calculator() {//function Principal
     const [result, setResult] = useState("");// def vars state 
     const [result1, setResult1] = useState("");
     const inputRef = useRef(0);
     var tempo;
-     
+
+    function handleClick(e) {// function for handle click in botton of calculator
+      tempo=e.target.value; //this value take of botton selected
+      setResult(result.concat(tempo)); //concate var "tempo" & "result"
+    }    
 
     function handleChange(e) {//handle change state of input
       setResult({value: e.target.value});
@@ -15,14 +18,32 @@ function Calculator() {//function Principal
 
  
     return ( /*return this value*/
-
-        <div >
-              {/*screen of calculator  */}
-            <div>
+ 
+            <div>{/*screen of calculator  */}
               <input type="text"   value={result} ref={inputRef}   onChange={handleChange}/>
             </div>
              
-            </div>
+            <div   className="keys">{/*bottons calculator*/}
+              <button value="+" onClick={handleClick} type="button">+</button>
+              <button value="-" onClick={handleClick} type="button">-</button>   
+              <button value="*" onClick={handleClick} type="button">&times;</button>        
+              <button value="/" onClick={handleClick} type="button">&divide;</button>        
+              
+              <button value="7" onClick={handleClick} type="button">7</button>
+              <button value="8" onClick={handleClick} type="button">8</button>
+              <button value="9" onClick={handleClick} type="button">9</button>
+              <button value="4" onClick={handleClick} type="button">4</button>
+              <button value="5" onClick={handleClick} type="button">5</button>
+              <button value="6" onClick={handleClick} type="button">6</button>
+              <button value="1" onClick={handleClick} type="button">1</button>
+              <button value="2" onClick={handleClick} type="button">2</button>
+              <button value="3" onClick={handleClick} type="button">3</button>
+              <button value="0" onClick={handleClick} type="button">0</button>
+              <button value="." onClick={handleClick} type="button">.</button>
+              
+              <button  id="clear" onClick={clear} type="button" >AC</button>
+              <button id="result" onClick={} type="button">=</button>
+           </div>
 
     );
   }
