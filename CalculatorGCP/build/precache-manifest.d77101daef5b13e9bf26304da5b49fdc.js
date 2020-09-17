cc8cb3faff1907e0d6efb1fc2c712b27a317fc41
jtrebolledom <jtrebolledom@gmail.com>
2020-09-17 17:32:35 -0300

    File app.yaml added and directory build

---
+++ CalculatorGCP/build/precache-manifest.d77101daef5b13e9bf26304da5b49fdc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34ac35fa1f92c6904fe15772a70ae31a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3cd51d979cac937b89c3",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "6485061f98528867fd54",
+    "url": "/static/css/main.b49dce47.chunk.css"
+  },
+  {
+    "revision": "3cd51d979cac937b89c3",
+    "url": "/static/js/2.3e64286b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.3e64286b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6485061f98528867fd54",
+    "url": "/static/js/main.b5208a1b.chunk.js"
+  },
+  {
+    "revision": "9e771e95d2c6f6d22f0b",
+    "url": "/static/js/runtime-main.bfc8c1f4.js"
+  }
+]);
